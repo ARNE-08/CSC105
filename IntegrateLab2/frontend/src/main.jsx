9cb5134266f399200e9f23b6642398bdc287ec8f
Paramita S <judge.27miyoo@gmail.com>
2023-05-17 11:59:16 +0700

    context

--- IntegrateLab2/frontend/src/main.jsx
+++ IntegrateLab2/frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { QueryClientProvider,QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { CommentContextProvider } from './share/Context/CommentContext';
 
 const queryClient = new QueryClient();
 
@@ -10,8 +11,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <CommentContextProvider>
+        <App />
+      </CommentContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
-  
+
 );
